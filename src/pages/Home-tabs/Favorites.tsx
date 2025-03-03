0b42a39c00f1cfe064141d3516b809e4e0964826
JABENIAO <jboyjabeniao@gmail.com>
2025-03-03 03:46:05 -1200

    Adding feed

--- src/pages/Home-tabs/Favorites.tsx
+++ src/pages/Home-tabs/Favorites.tsx
@@ -1,26 +1,62 @@
 import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  const Favorites: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Favorites</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  export default Favorites;
+  IonAvatar,
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonIcon, 
+  IonItem, 
+  IonLabel, 
+  IonList, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar 
+} from '@ionic/react';
+
+import { chatbubbleOutline } from 'ionicons/icons'; 
+
+const checkbox = [
+  { 
+    name: "Jebbie Jabeniao", 
+    img: "https://scontent.fmnl13-3.fna.fbcdn.net/v/t39.30808-6/319520929_823113845584818_1207869359198182760_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeEmokeOX2EGVcKkYjhF3PHPBHiPph60-VgEeI-mHrT5WFY7F2hIfGK5aF9t1Oi-HIM7yNpviuOczMtzmna4ENtp&_nc_ohc=OiqkAyohRfQQ7kNvgEru7Y3&_nc_oc=AdionPCshT0TOFjK-ENve3NtM1-6zlMd3HyyFwbqn1R0wdNrk6SuxBoAjHqiTg4d6NKzuPuJOUIkg_yf4ku5F5mf&_nc_zt=23&_nc_ht=scontent.fmnl13-3.fna&_nc_gid=AjC68AJmYDtCviy-_0xPqn2&oh=00_AYDC33-GukYs8ujjmagxsOERIrTMndkx1MEzYL6qNSwyPA&oe=67CA313F" 
+  },
+  { 
+    name: "Kobe ng Pinas", 
+    img: "https://cdn.allfamous.org/people/avatars/jonahrenz-jacob-ubwe-1626274304226-allfamous.org.jpg" 
+  },
+  { 
+    name: "Mr Long Bomb", 
+    img: "https://fastbreak.com.ph/wp-content/uploads/2025/02/Long-Bomb.jpg" 
+  },
+];
+
+const Favorites: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle> SUPERSTAR PLAYERS</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen color="light"> 
+        <IonList inset={true}>
+          {checkbox.map((contact, index) => ( 
+            <IonItem key={index} button>
+              <IonAvatar slot="start">
+                <img alt={contact.name} src={contact.img} />
+              </IonAvatar>
+              <IonLabel>{contact.name}</IonLabel>
+              <IonIcon icon={chatbubbleOutline} slot="end" size="large" color="primary" />
+            </IonItem>
+          ))}
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Favorites;
--- src/pages/Home-tabs/Feed.tsx
+++ src/pages/Home-tabs/Feed.tsx
@@ -1,26 +1,77 @@
 import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  const Feed: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Feed</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  export default Feed;
+  IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar 
+} from '@ionic/react';
+
+const subjects = [
+  { title: "Application Dev & Emerging Technologies",
+    subtitle: "Lecture - Wednesday 10:30 AM - 12:00 PM\nLaboratory - Monday 1:00 PM - 2:30 PM",
+    instructor: "Cliff Evangelio Fernandez",
+    color: "primary" },
+
+  { title: "Elective 4 Hardware Implementation Technologies",
+    subtitle: "Lecture - Wednesday 9:00 AM - 10:30 AM\nLaboratory - Wednesday 1:00 PM - 2:30 PM",
+    instructor: "Charlegene Bulahan Ogok",
+    color: "secondary" },
+
+  { title: "System Integration & Architecture",
+    subtitle: "Lecture - Thursday 9:00 AM - 10:30 AM",
+    instructor: "Phoebe B. Sudaria",
+    color: "tertiary" },
+
+  { title: "Phillipine Indigenous Communities",
+    subtitle: "Lecture - Monday 9:00 AM - 10:30 AM",
+    instructor: "Rose Ann Cuizoz",  
+    color: "success" },
+
+  { title: "People and Earth Ecosystem", 
+    subtitle: "Lecture - Tuesday 9:00 AM - 10:30 AM", 
+    instructor: "Alveen Gomez",  
+    color: "warning" },
+
+  { title: "Enterprise", 
+    subtitle: "Lecture - Friday 1:00 PM - 2:30 PM \n Laboratory - Thursday 2:30 PM - 4:00 PM",
+    instructor: "Cliff Evangelio Fernandez",
+    color: "danger" },
+];
+
+const Feed: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>SUBJECT SCHEDULES</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      
+      <IonContent fullscreen>
+        {subjects.map((subject, index) => (
+          <IonCard key={index} color={subject.color}>
+            <IonCardHeader>
+              <IonCardTitle>{subject.title}</IonCardTitle>
+              <IonCardSubtitle>{subject.subtitle}</IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <p><strong>Instructor:</strong> {subject.instructor}</p>
+            </IonCardContent>
+          </IonCard>
+        ))}
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Feed;
