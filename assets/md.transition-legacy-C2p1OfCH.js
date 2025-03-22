ca99679a123168ad3c73c082ae591da1a7c20d16
JABENIAO <jboyjabeniao@gmail.com>
2025-03-21 15:52:45 -1200

    Updates

--- assets/md.transition-legacy-C2p1OfCH.js
+++ assets/md.transition-legacy-C2p1OfCH.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B5Slyv_1.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-D-ZD0wK-.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
