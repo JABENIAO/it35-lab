77994b63e8956e9f047bbcdbadca76f6bf0ddae6
JABENIAO <jboyjabeniaO@gmail.com>
2025-04-04 18:28:44 -1200

    Updates

--- assets/ios.transition-legacy-xcQ45UB-.js
+++ assets/ios.transition-legacy-xcQ45UB-.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CHqYx-8_.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-7BpZw_hQ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
